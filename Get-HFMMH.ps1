b9d4b3133f77adb9b460da335e245edaefa72eb1
Philippe tulesauraspas <yanaski@msn.com>
2018-10-09 00:00:12 +0200

    creation of New-HFMHostsFileEntry and Set-HFMHostsFileEntry

--- Get-HFMMH.ps1
+++ Get-HFMMH.ps1
@@ -1,4 +1,20 @@
 Function Get-HFMHostsfile {
+    <#
+    .SYNOPSIS
+        Short description
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+
     [CmdletBinding()]
     Param
     (
@@ -21,6 +37,22 @@ Function Get-HFMHostsfile {
 }
 
 Function Get-HFMHostsFileContent {
+    <#
+    .SYNOPSIS
+        Short description
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+
     [CmdletBinding()]
     Param
     (
@@ -37,6 +69,91 @@ Function Get-HFMHostsFileContent {
 
     END {}
 }
+
+Function New-HFMHostsFileEntry {
+    <#
+    .SYNOPSIS
+        Short description
+    .DESCRIPTION
+        Long description
+    .EXAMPLE
+        PS C:\> <example usage>
+        Explanation of what the example does
+    .INPUTS
+        Inputs (if any)
+    .OUTPUTS
+        Output (if any)
+    .NOTES
+        General notes
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+        ## regex to match ip
+        [Parameter(Mandatory=$False)]
+        [String]$IpAddress,
+        ## regex, no space etc..
+        [Parameter(Mandatory=$False)]
+        [String]$HostName,
+        [Parameter(Mandatory=$False)]
+        [String]$FullyQualifiedName,
+        [Parameter(Mandatory=$False)]
+        [String]$Description,
+        [ValidateSet("Entry","Comment","BlankLine")]
+        [Parameter(Mandatory=$True)]
+        [String]$EntryType
+    )
+
+    Switch ($EntryType) {
+        BlankLine {
+            return [HostsEntry]::New()
+        }
+
+        Comment {
+            return [HostsEntry]::New($IpAddress,$HostName,$FullyQualifiedName,$Description,[HostsEntryType]::Comment)
+        }
+
+        Entry {
+            return [HostsEntry]::New($IpAddress,$HostName,$FullyQualifiedName,$Description,[HostsEntryType]::Entry)
+        }
+    }
+}
+
+Function Set-HFMHostsFileEntry {
+    <#
+     # Need Testing, hostfile is backedup and re-created
+     #>
+    [CmdletBinding(SupportsShouldProcess = $True, ConfirmImpact = 'High')]
+    Param
+    (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [HostsFile[]]$Path,
+        [Parameter(Mandatory=$True)]
+        [HostsEntry[]]$Entry
+
+    )
+
+    BEGIN{}
+
+    PROCESS{
+        If ($PSCmdlet.ShouldProcess($Path)) {
+            Write-Output 'Your hosts file will be backedup and re-created with the specified entries...'
+        }
+        $Path.AddHostsEntry($Entry)
+        $Path.Set()
+    }
+
+    END{}
+}
+
+<#
+    New-HFMHostsFileEntry -> Creates an HostFileEntry object (Of type [HostsEntry])
+    Set-HFMHostsFileEntry
+    Set-HFMHostsFileEntry ->
+    Save-HFMHostFileEntry
+    New-HFMHostsFileBackup
+#>
 
 <#
 Example 1:
