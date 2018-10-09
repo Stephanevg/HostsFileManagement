e0447f2327dd2615d050826606413770ff4ae025
CHASLES Philippe <pchasles@assystemtechnologies.com>
2018-10-09 14:28:50 +0200

    Added Comments

--- Get-HFMMH.ps1
+++ Get-HFMMH.ps1
@@ -82,3 +82,118 @@ Function Get-HFMHostsFileContent {
 
     END {}
 }
+
+Function New-HFMHostsFileEntry {
+    <#
+    .SYNOPSIS
+        Create a new Hosts File Entry.
+    .DESCRIPTION
+        Create a new Hosts File Entry.
+    .EXAMPLE
+        PS C:\> $NewEntry = New-HFMHostsFileEntry -IpAddress "20.0.0.0" -HostName "toto" -FullyQualifiedName "toto.com" -Description "ahahha" -EntryType Comment
+        Create a new comment entry.
+    .INPUTS
+        Mostly Strings.
+    .OUTPUTS
+        A [HostsEntry] object.
+    .NOTES
+        This cmdlet uses Class.HostsManagement classes, by @StephaneVG
+        Fork hist project if you like it: https://github.com/Stephanevg/Class.HostsManagement
+        Visit his site, and read his article a boute pratical use of PowerShell Classes: http://powershelldistrict.com/powershell-class/
+    #>
+
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$False)]
+        [String]$IpAddress,
+        [Parameter(Mandatory=$False)]
+        [String]$HostName,
+        [Parameter(Mandatory=$False)]
+        [String]$FullyQualifiedName,
+        [Parameter(Mandatory=$False)]
+        [String]$Description,
+        [ValidateSet("Entry","Comment","BlankLine")]
+        [Parameter(Mandatory=$True)]
+        [String]$EntryType
+    )
+
+    Switch ($EntryType) {
+        BlankLine {
+            return [HostsEntry]::New()
+        }
+
+        Comment {
+            return [HostsEntry]::New($IpAddress,$HostName,$FullyQualifiedName,$Description,[HostsEntryType]::Comment)
+        }
+
+        Entry {
+            return [HostsEntry]::New($IpAddress,$HostName,$FullyQualifiedName,$Description,[HostsEntryType]::Entry)
+        }
+    }
+}
+
+Function Set-HFMHostsFileEntry {
+    <#
+    .SYNOPSIS
+        Add new entry/entries to hosts file.
+    .DESCRIPTION
+        Add new entry/entries to hosts file(s)
+    .EXAMPLE
+        PS C:\> $Host = Get-HFMHostsfile
+        PS C:\> $Comment1 = New-HFMHostsFileEntry -IpAddress "20.0.0.0" -HostName "toto" -FullyQualifiedName "toto.com" -Description "ahahha" -EntryType Comment
+        PS C:\> $Comment2 = New-HFMHostsFileEntry -IpAddress "21.0.0.0" -HostName "toto" -FullyQualifiedName "toto.com" -Description "ahahha" -EntryType Comment
+        PS C:\> Set-HFMHostsFileEntry -Path $Host -Entries $Comment1,$Comment2
+
+        Create 2 new [HostsEntry], and add them to the local hosts file.
+        A backup is automatically created.
+    .EXAMPLE
+        PS C:\> $Comment1 = New-HFMHostsFileEntry -IpAddress "20.0.0.0" -HostName "toto" -FullyQualifiedName "toto.com" -Description "ahahha" -EntryType Comment
+        PS C:\> "Computer1","Computer2" | Get-HFMHostsfile | Set-HFMHostsFileEntry -Entries $Comment1
+
+        Create a new [HostsEntry], and add it to hostsfile on computer1, and computer2
+    .INPUTS
+        You must provide a valid path of type [HostsFile] and an entry of type [HostsEntry]
+    .OUTPUTS
+        
+    .NOTES
+        This cmdlet uses Class.HostsManagement classes, by @StephaneVG
+        Fork hist project if you like it: https://github.com/Stephanevg/Class.HostsManagement
+        Visit his site, and read his article a boute pratical use of PowerShell Classes: http://powershelldistrict.com/powershell-class/
+    #>
+    Param
+    (
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+        [HostsFile[]]$Path,
+        [Parameter(Mandatory=$True)]
+        [HostsEntry[]]$Entries
+    )
+
+    BEGIN{}
+
+    PROCESS{
+
+        Foreach ($File in $path ) {
+            $File.ReadHostsFileContent()
+            $File.AddHostsEntry($Entries)
+            $File.Set()
+        }
+    }
+
+    END{}
+}
+
+<#
+Remove-Module Get-HFMMH;Import-Module .\Get-HFMMH.ps1;
+$h = Get-HFMHostsfile
+$n = New-HFMHostsFileEntry -IpAddress "20.0.0.0" -HostName "toto" -FullyQualifiedName "toto.com" -Description "ahahha" -EntryType Comment
+$m = New-HFMHostsFileEntry -IpAddress "21.0.0.0" -HostName "toto" -FullyQualifiedName "toto.com" -Description "ahahha" -EntryType Comment
+Set-HFMHostsFileEntry -Path $h -Entries $n,$m
+
+$h.GetEntries()
+$h.addHostEntries()
+$h.set()
+
+"localhost","L-FRA-370397" | Get-HFMHostsfile | Set-HFMHostsFileEntry -Entries $n,$m
+
+#>
